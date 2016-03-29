8cb6f2cb51ff51e2c6088dd4d3706925829c4912
Mathieu Goeminne <mathieu.goeminne@cetic.be>
2016-03-29 10:55:36 +0200

    ARMA is now an immutable model that can generate stream of values

--- src/main/scala/be/cetic/tsgen/Main.scala
+++ src/main/scala/be/cetic/tsgen/Main.scala
@@ -3,6 +3,9 @@ package be.cetic.tsgen
 object Main
 {
   def main(args: Array[String]) {
-    ARMA.series(phi=Array(0.5), theta=Array(0.5), std=0.5, c=0).take(500) foreach(e => println(e.formatted("%2f")))
+
+    val model = ARMA(std=0.1, c=0)
+
+    model.series take 500 foreach(e => println(e.formatted("%2f")))
   }
 }
