c276c052a46b70be93e88fe86b450c1affda1dd5
Ahah <mg@macbook-mg.local>
2016-04-01 14:36:49 +0200

    The random path noise is now a regular time series generator, NoisyCyclicTimeSeries is no longer useful.

--- src/main/scala/be/cetic/tsgen/CompositeTimeSeries.scala
+++ src/main/scala/be/cetic/tsgen/CompositeTimeSeries.scala
@@ -6,8 +6,14 @@ import org.joda.time.LocalDateTime
   * This time series generator is a composition of other generators.
   * The values provided by each generator are summed.
   */
-case class CompositeTimeSeriesGenerator(generators: Seq[TimeSeriesGenerator]) extends TimeSeriesGenerator
+class CompositeTimeSeries(val generators: Seq[TimeSeriesGenerator]) extends TimeSeriesGenerator
 {
    override def compute(times: Stream[LocalDateTime]): Stream[Double] = generators.map(c => c.compute(times))
                                                                                   .reduce((a,b) => a.zip(b).map(e => e._1 + e._2))
+}
+
+
+object CompositeTimeSeries
+{
+   def apply(generators: TimeSeriesGenerator*): CompositeTimeSeries = new CompositeTimeSeries(generators)
 }
--- src/main/scala/be/cetic/tsgen/TimeSeriesGenerator.scala
+++ src/main/scala/be/cetic/tsgen/TimeSeriesGenerator.scala
@@ -8,7 +8,8 @@ import org.joda.time.LocalDateTime
 trait TimeSeriesGenerator
 {
    /**
-     * @param times a series of time for which values must be computed.
+     * @param times a series of time for which values must be computed. Each time must be greater than or equal to
+     *              the previous one.
      * @return the values associated to the specified times.
      */
    def compute(times: Stream[LocalDateTime]): Stream[Double]
