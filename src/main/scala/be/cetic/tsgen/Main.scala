5e312ec7cc951ab8c471a94c38deaff60f7557ce
Mathieu Goeminne <mgoeminne@gmail.com>
2016-04-11 15:33:44 +0200

    Time series can now be generated by using a configuration file.

--- src/main/scala/be/cetic/tsgen/Main.scala
+++ src/main/scala/be/cetic/tsgen/Main.scala
@@ -1,13 +1,18 @@
 package be.cetic.tsgen
 
+import be.cetic.tsgen.config.Configuration
 import com.github.nscala_time.time.Imports._
 import org.joda.time.DateTimeConstants
+import spray.json._
+import DefaultJsonProtocol._
+import be.cetic.tsgen.config.GeneratorLeafFormat._
 
 
 object Main
 {
    /**
      * Creates a sequence of times, from a start time to a given limit.
+     *
      * @param start the start time.
      * @param end the end time, no retrieved time can be set after this time.
      * @param duration the time space between two consecutive times.
@@ -20,6 +25,7 @@ object Main
 
    /**
      * Creates a sequence of times, from a start time to a given limit.
+     *
      * @param start the start time.
      * @param end the end time, no retrieved time can be set after this time.
      * @param nbTimes the number of times that must be retrieved
@@ -79,19 +85,97 @@ object Main
          2021 -> 5
       ))
 
-      val noise = RandomWalkTimeSeries(ARMA(std=0.5, theta = Array(1)), timeStep = Duration.standardHours(1))
+      val document =
+         """
+           |{
+           |   "generators": [
+           |      {
+           |         "name": "daily-generator",
+           |         "type": "daily",
+           |         "points": {"10:00:00.000": 12, "17:00:00.000": 15, "20:00:00.000": 12}
+           |      },
+           |      {
+           |         "name": "noisy-daily",
+           |         "type": "aggregate",
+           |         "aggregator": "sum",
+           |         "generators": [
+           |            "daily-generator",
+           |            {
+           |                "type": "arma",
+           |                "model": { "std": 0.1, "c": 0, "seed": 159357},
+           |                "timestep": 3600000
+           |            }
+           |         ]
+           |      },
+           |      {
+           |         "name":  "partial-daily",
+           |         "type": "partial",
+           |         "generator": "daily-generator",
+           |         "from": "2016-01-01 00:00:00.000",
+           |         "to": "2017-01-01 00:00:00.000"
+           |      },
+           |      {
+           |         "name":  "logistic-daily",
+           |         "type": "logistic",
+           |         "generator": "daily-generator",
+           |         "location": 13,
+           |         "scale": 8
+           |      }
+           |   ],
+           |   "series": [
+           |      {
+           |         "name": "series-A",
+           |         "generator": "daily-generator",
+           |         "frequency": 300000
+           |      },
+           |      {
+           |         "name": "series-B",
+           |         "generator": "logistic-daily",
+           |         "frequency": 300000
+           |      }
+           |   ],
+           |   "from": "2016-01-01 00:00:00.000",
+           |   "to": "2016-01-07 00:00:00.000"
+           |}
+         """.stripMargin.parseJson
 
+      val config = document.convertTo[Configuration]
 
-      val times: Stream[LocalDateTime] = sampling( new LocalDateTime(2016,1,1,0,0,0),
-                                                   new LocalDateTime(2016,12,31,23,59,59),
-                                                   Duration.standardHours(1))
+      println("date;series;value")
 
-      val gen = DailyTimeSeries(Map(
-         new LocalTime(11, 0, 0) -> 6,
-         new LocalTime(13, 0, 0) -> 8,
-         new LocalTime(17, 0, 0) -> 13,
-         new LocalTime(21, 0, 0) -> 3))
+      generate(config2TimeSeries(config)) foreach (e => println(dtf.print(e._1) + ";" + e._2 + ";" + e._3))
+   }
 
+   private def config2TimeSeries(config: Configuration): Map[String, Stream[(LocalDateTime, Any)]] =
+   {
+      val ts = config.timeSeries
 
+      ts.map(series =>
+      {
+         val name = series._1
+         val frequency = series._2._2
+         val values = series._2._1
+         val times = Main.sampling(config.from, config.to, frequency)
+
+         (name -> values.compute(times).filter(e => e._2.isDefined).map(e => (e._1, e._2.get)))
+      })
+   }
+
+   private def generate(series: Map[String, Stream[(LocalDateTime, Any)]]): Stream[(LocalDateTime, String, Any)] =
+   {
+      val cleanedMap = series.filterNot(_._2.isEmpty)
+
+      if (cleanedMap.isEmpty) Stream.Empty
+      else
+      {
+         val selected = cleanedMap.minBy(e => e._2.head._1)
+
+         val head = selected._2.head
+         val tail = selected._2.tail
+
+         val next = series.updated(selected._1, tail)
+
+         (head._1, selected._1, head._2) #:: generate(next)
+      }
    }
 }
